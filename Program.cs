3dbea3a921f39dec32f4f057427c60e930e5005c
juhriber <j.de.s.r.25@gmail.com>
2021-03-08 13:41:09 -0300

    Finalização do projeto Gritador

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string qualquerCoisa;
+
+            //<---Inicio(Digite)--->//
+            Console.Write("Digite qualquer coisa: ");
+            qualquerCoisa = Console.ReadLine();
+
+            Console.WriteLine($"{qualquerCoisa}!!!".ToUpper());
+            //<---Fim(Digite)--->//
+
+            //<---Inicio(Sair)--->//
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            //<---Fim(Sair)--->// 
+        }
+    }
+}
